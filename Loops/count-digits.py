5e1df19c3ce36befe06bc799360db99705add142
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-09-09 19:12:06 +0530

    count digits

---
+++ Loops/count-digits.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number : "))
+count = 0
+while n>0:
+    n = n//10
+    count=count+1
+print(count)    
+
+
+
+
+
+# Method 2
+# n = int(input("Enter a number : "))
+# print("Total digits =", len(str(n)))
